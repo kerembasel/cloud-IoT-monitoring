140354eed32b0f192d71fed07046926a83fa27be
kerembasel <160721207+kerembasel@users.noreply.github.com>
2025-10-05 21:34:01 +0300

    Update sensor_simulator.py

--- simulator/sensor_simulator.py
+++ simulator/sensor_simulator.py
@@ -1,10 +1,8 @@
 import random, time, os, requests
 
-# Docker/K8s içinde servis adıyla erişim için 'api' kullanacağız.
 API_URL = os.getenv("API_URL", "http://api:5000/data")
 
 while True:
-    # Rastgele ölçümler üret
     data = {
         "temperature": round(random.uniform(20, 35), 2),
         "humidity": round(random.uniform(40, 70), 2),
@@ -12,9 +10,7 @@ while True:
     }
     print("Sending:", data, "->", API_URL, flush=True)
     try:
-        # 2 sn'de bir ölçüm yolla
         requests.post(API_URL, json=data, timeout=2)
     except Exception as e:
-        # API düşmüşse/log takibi için
         print("Post failed:", e, flush=True)
     time.sleep(2)
